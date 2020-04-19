bf9eb247aa4deac4255def9c0417cde0146325e1
Bello Yakub <hyaaquub@gmail.com>
2020-04-19 21:43:37 +0100

    Done with list items

---
+++ app/src/main/java/com/example/grocerieslist/checkout_page.java
@@ -0,0 +1,14 @@
+package com.example.grocerieslist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class checkout_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout_page);
+    }
+}
