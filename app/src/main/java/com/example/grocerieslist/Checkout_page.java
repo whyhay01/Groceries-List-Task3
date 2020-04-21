6c269199da0d50ed0c52af0ce1769c51000d5352
Bello Yakub <hyaaquub@gmail.com>
2020-04-21 10:15:26 +0100

    Done with second layout

--- app/src/main/java/com/example/grocerieslist/Checkout_page.java
+++ app/src/main/java/com/example/grocerieslist/Checkout_page.java
@@ -3,12 +3,18 @@ package com.example.grocerieslist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class Checkout_page extends AppCompatActivity {
+    ListView listView;
+    TextView mTextview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout_page);
+        listView = findViewById(R.id.list_view);
+        mTextview = findViewById(R.id.txt_total);
     }
 }
